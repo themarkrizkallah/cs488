97236a0bc87a64cc61c227955598a0f96819de9a
Mark Rizkallah <mark.g.rizkallah@gmail.com>
2020-06-08 23:31:33 -0400

    Start A2

--- A2/Main.cpp
+++ A2/Main.cpp
@@ -1,9 +1,9 @@
-// Winter 2020
+// Spring 2020
 
 #include "A2.hpp"
 
 int main( int argc, char **argv ) 
 {
-	CS488Window::launch( argc, argv, new A2(), 768, 768, "F19 Assignment 2" );
+	CS488Window::launch( argc, argv, new A2(), 768, 768, "S20 Assignment 2" );
 	return 0;
 }
